4d688be94c1f6995c7c918e13a7c33ab14c88aec
claraaqwq <128831601+claraaqwq@users.noreply.github.com>
2025-10-20 20:16:24 -0500

    sda

---
+++ backend/src/main/java/com/system/morapack/dao/morapack_psql/model/TrackingEventType.java
@@ -0,0 +1,5 @@
+package com.system.morapack.dao.morapack_psql.model;
+
+public enum TrackingEventType {
+  CREATED, PICKED_UP, IN_TRANSIT, ARRIVED_HUB, DEPARTED_HUB, DELAYED, OUT_FOR_DELIVERY, DELIVERED, CANCELLED
+}
